c05a2b7746b4d9ec0ad60d252f9302c8d226ef3f
blazeowner1414 <vikashk96573@gmail.com>
2025-11-28 15:11:56 +0530

    remove logger admin checks

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -3,12 +3,12 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 
 import config
 
-from ..logging import LOGGER
+# from .logging import LOGGER
 
 
 class Anony(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        print("Starting Bot...")
         super().__init__(
             name="AnonXMusic",
             api_id=config.API_ID,
@@ -26,29 +26,11 @@ class Anony(Client):
         self.username = self.me.username
         self.mention = self.me.mention
 
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-            )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
-
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
-            exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+        # a = await self.get_chat_member(config.LOGGER_ID, self.id)
+# if a.status != ChatMemberStatus.ADMINISTRATOR:
+#     exit()
+        
+        print(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
